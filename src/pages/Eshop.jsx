905a05e578792e0c0e6c96f58e33974f53e9f0fd
Zo Nirina <tomzonirina@gmail.com>
2023-10-03 17:57:13 +0300

    feat: adding range

--- src/pages/Eshop.jsx
+++ src/pages/Eshop.jsx
@@ -53,6 +53,22 @@ return (
                         </section>
                     </div>
                 </div>
+                <section className='dropdownResult'>
+                    <section className='contentDropdown'>
+                        <article className='heading'>
+                            <h2>Prix</h2>
+                        </article>
+                        <section className='contentRange'>
+                            <input type="range" name="prix" id="prix" list="prix" />
+                            <datalist id="prix">
+                                <option value="0" label="0€"></option>
+                                <option value="100" label="100"></option>
+                                <option value="200" label="200€"></option>
+                            </datalist>
+                        </section>
+                    </section>
+                    <section className='contentCard'></section>
+                </section>
             </section>
         </Element>
     </main>

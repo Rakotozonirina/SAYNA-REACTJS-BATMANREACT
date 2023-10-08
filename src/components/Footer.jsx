b4bf85c03983a37c44e1db29f4c956b0c08c63cb
Zo Nirina <tomzonirina@gmail.com>
2023-10-08 18:58:55 +0300

    feat: adding of the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+function Footer() {
+return (
+    <footer>
+        <article className='footer-article'>
+            <div className='footer-wb'></div>
+            <p>Ce projet respecte l’univers cinématographique des films Batman récents. Version Nolan & Snyder & Reeves</p>
+        </article>
+        <section className='footer-section'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </section>
+    </footer>
+)
+}
+
+export default Footer
--- src/pages/Eshop.jsx
+++ src/pages/Eshop.jsx
@@ -9,77 +9,81 @@ import Fleche from '../photos/logos/flèche_down_header.png'
 import CustomSelect from '../components/CustomSelect'
 import AnotherSelect from '../components/AnotherSelect'
 import Card from '../components/Card'
+import Footer from '../components/Footer'
 
 function Eshop() {
 return (
-    <main className="mainStyle">
-        <Navbar/>
-        <Element name='top'>
-            <div className='colorText'>
-                <article className='articleStyle'>
-                    <h1>récuperez le flow de <br /> batman avec notre e-shop!</h1>
-                </article>
-                <section>
-                    <Icons/>
-                </section>
-            </div>
-        </Element>
-        <Element name='showResult'>
-            <section className='showResult'>
-                <div className='cartDiv'>
-                    <section className='sectionFirst'>
-                        <ScrollLink to="showResult" spy={true} smooth={true} duration={500}>
-                            <img src={Fleche} alt="flèche down" />
-                        </ScrollLink>
+    <>
+        <main className="mainStyle">
+            <Navbar/>
+            <Element name='top'>
+                <div className='colorText'>
+                    <article className='articleStyle'>
+                        <h1>récuperez le flow de <br /> batman avec notre e-shop!</h1>
+                    </article>
+                    <section>
+                        <Icons/>
                     </section>
-                    <div className='DivTwo'>
-                        <article>
-                            <h2>Filtres</h2>
-                        </article>
-                        <section>
-                            <CartComponent productsCount={1} />
-                        </section>
-                    </div>
                 </div>
-                <section className='dropdownResult'>
-                    <section className='contentDropdown'>
-                        <article className='heading'>
-                            <h2>Prix</h2>
-                        </article>
-                        <section className='contentRange'>
-                            <input type="range" name="prix" id="prix" list="prix" />
-                            <datalist id="prix">
-                                <option value="0" label="0€"></option>
-                                <option value="100" label="100"></option>
-                                <option value="200" label="200€"></option>
-                            </datalist>
+            </Element>
+            <Element name='showResult'>
+                <section className='showResult'>
+                    <div className='cartDiv'>
+                        <section className='sectionFirst'>
+                            <ScrollLink to="showResult" spy={true} smooth={true} duration={500}>
+                                <img src={Fleche} alt="flèche down" />
+                            </ScrollLink>
                         </section>
-                        <section>
-                            <CustomSelect />
+                        <div className='DivTwo'>
+                            <article>
+                                <h2>Filtres</h2>
+                            </article>
+                            <section>
+                                <CartComponent productsCount={1} />
+                            </section>
+                        </div>
+                    </div>
+                    <section className='dropdownResult'>
+                        <section className='contentDropdown'>
+                            <article className='heading'>
+                                <h2>Prix</h2>
+                            </article>
+                            <section className='contentRange'>
+                                <input type="range" name="prix" id="prix" list="prix" />
+                                <datalist id="prix">
+                                    <option value="0" label="0€"></option>
+                                    <option value="100" label="100"></option>
+                                    <option value="200" label="200€"></option>
+                                </datalist>
+                            </section>
+                            <section>
+                                <CustomSelect />
+                            </section>
+                            <section className='color'>
+                                <div className='contentColor'>
+                                    Couleur
+                                    <span> <FaAngleDown/> </span>
+                                </div>
+                            </section>
+                            <section className='anotherSelect'>
+                                <AnotherSelect/>
+                            </section>
                         </section>
-                        <section className='color'>
-                            <div className='contentColor'>
-                                Couleur
-                                <span> <FaAngleDown/> </span>
-                            </div>
-                        </section>
-                        <section className='anotherSelect'>
-                            <AnotherSelect/>
-                        </section>
-                    </section>
-                    <section className='contentCard'>
-                        <section className='contentTrier'>
-                            <h2 className='result-one'>Résultat: 122</h2>
-                            <p className='result-two'> Trier par <span> <FaAngleDown className='icon'/> </span></p>
-                        </section>
-                        <section className='content-cards'>
-                            <Card/>
+                        <section className='contentCard'>
+                            <section className='contentTrier'>
+                                <h2 className='result-one'>Résultat: 122</h2>
+                                <p className='result-two'> Trier par <span> <FaAngleDown className='icon'/> </span></p>
+                            </section>
+                            <section className='content-cards'>
+                                <Card/>
+                            </section>
                         </section>
                     </section>
                 </section>
-            </section>
-        </Element>
-    </main>
+            </Element>
+            <Footer/>
+        </main>
+    </>
 )
 }
 
